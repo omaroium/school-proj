c8ec51b50fe3352672918639249f0cbf4abfc5bf
omaroium <96143894+omaroium@users.noreply.github.com>
2022-08-18 09:43:52 +0300

    updated chat works like does not

--- app.py
+++ app.py
@@ -11,15 +11,15 @@ app.config['SECRET_KEY'] = 'super-secret-key'
 
 
 Config = {
-  "apiKey": "AIzaSyBW232n5Lnogln0UNFG4cK6D-KScVGfexU",
-  "authDomain": "finally-4dd8f.firebaseapp.com",
-  "databaseURL": "https://finally-4dd8f-default-rtdb.europe-west1.firebasedatabase.app",
-  "projectId": "finally-4dd8f",
-  "storageBucket": "finally-4dd8f.appspot.com",
-  "messagingSenderId": "112476821739",
-  "appId": "1:112476821739:web:9023ddd0cb5ff6c92746ae",
-  "measurementId": "G-NYBE7M8ST7",
-  "databaseURL":"https://finally-4dd8f-default-rtdb.europe-west1.firebasedatabase.app/"
+  "apiKey": "AIzaSyB8mcn93dHFC0JumKFYDdvlEr_R_ZMIQE8",
+  "authDomain": "school-finalproj.firebaseapp.com",
+  "databaseURL": "https://school-finalproj-default-rtdb.europe-west1.firebasedatabase.app",
+  "projectId": "school-finalproj",
+  "storageBucket": "school-finalproj.appspot.com",
+  "messagingSenderId": "157803470609",
+  "appId": "1:157803470609:web:71301de1b5d0c9d0e5f6b0",
+  "measurementId": "G-BWZ6326WFZ",
+  "databaseURL":"https://school-finalproj-default-rtdb.europe-west1.firebasedatabase.app/"
 }
 
 firebase=pyrebase.initialize_app(Config)
@@ -82,7 +82,7 @@ def all_tweet():
         except:
             print("Couldn't add article")
             redirect(url_for('add_tweet'))
-    return render_template("tweets.html",tweets2=db.child("Tweets").get().val(),users=db.child("Users").get().val(), current_user=login_session['user']['localId'])
+    return render_template("tweets.html",tweets2=db.child("Tweets").child().get().val(),users=db.child("Users").get().val(), current_user=login_session['user']['localId'])
 
 @app.route('/sign_out', methods=['GET', 'POST'])
 def sign_out():
@@ -94,7 +94,7 @@ def like(k):
     if request.method == 'POST':
         try:
             likes = {'likes' : db.child('Tweets').child(k).get().val()['likes'] + 1}
-            db.child("Tweets").child(k).update(likes)
+            db.child("Tweets").child(k).update(likes')
             return redirect(url_for('all_tweet'))
         except:
             return redirect(url_for('all_tweet'))
@@ -104,27 +104,36 @@ def like(k):
 @app.route('/all_tweets/<string:user>', methods=['GET', 'POST'])
 def page(user):
     utweets=[]
-
+    hi=user
     tweet=db.child("Tweets").get().val()
     for x in tweet:
         if(tweet[x]['uid']==user):
            utweets.append(tweet[x])
-    return render_template('user_page.html',tweets2=utweets, length=len(utweets),users=db.child("Users").get().val(),user=user,current_user=login_session['user']['localId'])
+    return render_template('user_page.html',tweets2=utweets, length=len(utweets),users=db.child("Users").get().val(),user=hi,current_user=login_session['user']['localId'])
 
 
-
+@app.route('/massage/<string:other>', methods=['GET', 'POST'])
+def massage1(other):
+    if request.method == 'POST':
+       try:
+           now = datetime.now()
+           massage={"title":request.form['title'],"text":request.form['text'], "uid": login_session['user']['localId'],"uido":other,"time":now.strftime("%d/%m/%Y %H"),"likes":0,"img":request.form['img'] }
+           db.child("Masseges").push(massage)
+       except:
+           print("Couldn't add article")
+    return render_template("masseges.html",   massages=db.child("Masseges").get().val() ,current_user=login_session['user']['localId'] ,user=other
+)
 
 @app.route('/massage/<string:name>/<string:other>', methods=['GET', 'POST'])
 def massage(name,other):
-    utweets=[]
-    tweet={"title":request.form['title'],"text":request.form['text'], "uido": other,"time":now.strftime("%d/%m/%Y %H"),"likes":0,"img":request.form['img'] }
-    db.child('Massage').push(tweet)
-    tweet=db.child("Massages").get().val()
-    for x in tweet:
-        if(tweet[x]['uid']==name and tweet[x]['uido']==other):
-           utweets.append(tweet[x])
+    tweets=[]
+    masseges=db.child("Masseges").get().val()
+    for x in masseges:
+        if masseges[x]['uid']==name and masseges[x]['uido']==other:
+            tweets.append(masseges[x])
 
-    return render_template("session.html",    users=db.child("Users").get().val(),len=len(utweets),tweets2=utweets ,current_user=login_session['user']['localId'],user=name,other=other
-)
+
+    return render_template("session.html",tweets=tweets)
+
 if __name__ == '__main__':
     app.run(debug=True)
